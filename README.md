4aee741c59f7a90250852a1c473575a607db3ed6
sarehsoltani <sare.soltani74@gmail.com>
2025-12-09 13:20:01 -0800

    setup and install requirements

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Medibot
+# Medibot: LLM Medical Chatbot 
+
+### STEP 1
+
+'''bash
+conda create -n medibot
+'''
+
+'''bash
+conda activate medibot
+'''
---
+++ setup.py
@@ -0,0 +1,9 @@
+from setup import setup, find_packages
+setup(
+    name="medical_chatbot",
+    version="1.0.0",
+    author="Sareh Soltani Nejad",
+    author_email="sareh.soltani74@gmail.com",
+    packages = find_packages(),
+    install_requires=[]
+)
